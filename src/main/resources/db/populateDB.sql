3400241eb2b58bc02df3309f7d7a40f02340a16b
oldmermaidman <a.v.nikolaef@gmail.com>
2022-02-23 01:05:19 +0300

    HW03 base

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -10,3 +11,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, now(), 'еда админа 1', 500),
+       (100000, now(), 'еда админа 2', 600),
+       (100000, now(), 'еда админа 3', 700),
+       (100000, now(), 'еда админа 4', 800),
+       (100001, now(), 'еда юзера 1', 100),
+       (100001, now(), 'еда юзера 2', 200),
+       (100001, now(), 'еда юзера 3', 300),
+       (100001, now(), 'еда юзера 4', 400),
+       (100001, now(), 'еда юзера 5', 500);
